5b322e4cb252792a0d5051e2353d6bd7f3574c04
Matthew Thornton <matthewthornton.usa@gmail.com>
2016-03-18 16:15:41 -0700

    remove timeout

--- server/routes/standard.js
+++ server/routes/standard.js
@@ -7,19 +7,13 @@ module.exports = function(app) {
   });
 
   app.post('/standards', function(req, res) {
-    // Add a delay here to simulate the delay of a live server
-    // So things like button isSubmitting states can be demonstrated
-    setTimeout(function(){
       res.json(Standard.create(req.body));
-    }, 1000);
+
   });
 
   app.put('/standards/:id', function(req, res) {
-    // Add a delay here to simulate the delay of a live server
-    // So things like button isSubmitting states can be demonstrated
-    setTimeout(function(){
       res.json(Standard.update(req.body));
-    },1000)
+
   });
 
   app.get('/standards/:id', function(req, res) {
